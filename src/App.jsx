40647453978d1e77da04a2d5ba6c0fe3082d7937
Justen Manni <me@justenmanni.dev>
2023-06-14 22:46:56 +0800

    Add localStorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  // state
+  const [cartItems, setCartItems] = useState([]);
+
+  // useEffect to fetch cart items from localStorage
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
+
+  // Add items to Cart and push to localStorage
+  const addToCart = (item) => {
+    const updatedCart = [...cartItems, item];
+    setCartItems(updatedCart);
+    // push to localStorage and convert array to JSON string
+    localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+  };
+
+  // Remove items from cart and clear localStorage
+  const clearCart = () => {
+    setCartItems([]);
+    localStorage.removeItem("cartItems");
+  };
+
   return (
     <div>
       <h1>Cart App</h1>
+      <button onClick={() => addToCart("Item 1")}>Click to add - Item 1</button>
+      <button onClick={() => addToCart("Item 2")}>Click to add - Item 2</button>
+      <button onClick={() => addToCart("Item 3")}>Click to add - Item 3</button>
+      <button onClick={clearCart}>Clear Cart</button>
+      <h2>Cart Items:</h2>
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
